fc3f6bbd4b03919aaca7ef650ac8f96c17f68849
Phil Hash <ht.phi99@gmail.com>
2024-07-11 15:28:01 +0700

    Adding delete user function

--- qltv/admin/reject.php
+++ qltv/admin/reject.php
@@ -1,19 +1,23 @@
 <?php
 require('dbconn.php');
 
-$rollno=$_GET['id'];
+$bookid=$_GET['id1'];
 
-$sql="delete from LMS.user where RollNo='$rollno'";
+$rollno=$_GET['id2'];
+
+$sql="delete from LMS.record where RollNo='$rollno' and BookId='$bookid'";
 
 if($conn->query($sql) === TRUE)
 {
+	$sql1="insert into LMS.message (RollNo,Msg,Date,Time) values ('$rollno','Yêu cầu mượn sách với id: $bookid  bị từ chối',curdate(),curtime())";
+ $result=$conn->query($sql1);
 echo "<script type='text/javascript'>alert('Thành công')</script>";
-header( "Refresh:0.01; url=student.php", true, 303);
+header( "Refresh:0.01; url=issue_requests.php", true, 303);
 }
 else
 {
 	echo "<script type='text/javascript'>alert('Error')</script>";
-    header( "Refresh:0.01; url=student.php", true, 303);
+    header( "Refresh:0.01; url=issue_requests.php", true, 303);
 
 }
 
---
+++ qltv/admin/remove_student.php
@@ -0,0 +1,23 @@
+<?php
+require('dbconn.php');
+
+$rollno=$_GET['id'];
+
+$sql="delete from LMS.user where RollNo='$rollno'";
+
+if($conn->query($sql) === TRUE)
+{
+echo "<script type='text/javascript'>alert('Thành công')</script>";
+header( "Refresh:0.01; url=student.php", true, 303);
+}
+else
+{
+	echo "<script type='text/javascript'>alert('Error')</script>";
+    header( "Refresh:0.01; url=student.php", true, 303);
+
+}
+
+
+
+
+?>
