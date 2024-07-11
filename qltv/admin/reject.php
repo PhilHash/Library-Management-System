d0e4a5eeefeb1ea5ba9bafb64c5491d72d3e3e87
Phil Hash <ht.phi99@gmail.com>
2024-07-11 15:25:44 +0700

    Adding delete user function

--- qltv/admin/reject.php
+++ qltv/admin/reject.php
@@ -8,12 +8,12 @@ $sql="delete from LMS.user where RollNo='$rollno'";
 if($conn->query($sql) === TRUE)
 {
 echo "<script type='text/javascript'>alert('Thành công')</script>";
-header( "Refresh:0.01; url=issue_requests.php", true, 303);
+header( "Refresh:0.01; url=student.php", true, 303);
 }
 else
 {
 	echo "<script type='text/javascript'>alert('Error')</script>";
-    header( "Refresh:0.01; url=issue_requests.php", true, 303);
+    header( "Refresh:0.01; url=student.php", true, 303);
 
 }
 
