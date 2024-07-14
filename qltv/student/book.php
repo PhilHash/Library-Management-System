0477e0cfdea6ba1a49c8207e6f60df700a8fe0b5
Phil Hash <ht.phi99@gmail.com>
2024-07-15 06:41:54 +0700

    minor changes

--- qltv/student/book.php
+++ qltv/student/book.php
@@ -105,7 +105,7 @@ s0.parentNode.insertBefore(s1,s0);
                                     $rowcount=mysqli_num_rows($result);
 
                                     if(!($rowcount))
-                                        echo "<br><center><h2><b><i>No Results</i></b></h2></center>";
+                                        echo "<br><center><h2><b><i>Không kết quả</i></b></h2></center>";
                                     else
                                     {
 
